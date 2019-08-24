85e894d4e90cb07153157eb5c4d908a202592217
Sangamithrathiru15 <sangamithra.thirugnanam@gmail.com>
2019-08-24 23:09:43 +0530

    committing added login api changes

--- profile_api/urls.py
+++ profile_api/urls.py
@@ -5,9 +5,10 @@ from profile_api import views
 router=DefaultRouter()#assign the router to a variable
 router.register('hello-viewset',views.HelloViewSet,base_name="hello-view-set")#nam of the url,#the view set that needs to be registered
 #to register specific viewset with the router
-router.register('profile',views.UserProfileViewset)#base_name is not required since queryset was usedfor retrieving the values since the values in the query set will have the names, if we wish to overwrite then base_name can be defined 
+router.register('profile',views.UserProfileViewset)#base_name is not required since queryset was usedfor retrieving the values since the values in the query set will have the names, if we wish to overwrite then base_name can be defined
 
 urlpatterns=[
 path('hello-view/',views.HelloApiView.as_view()),#as_view is the function(class method) which will connect my MyView class with its url.
+path('login/',views.UserLoginAPIView.as_view()),
 path('',include(router.urls)),
 ]
--- profile_api/views.py
+++ profile_api/views.py
@@ -7,6 +7,8 @@ from profile_api import models
 from rest_framework.authentication import TokenAuthentication
 from profile_api import permissions
 from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 
 class HelloApiView(APIView):
@@ -109,3 +111,7 @@ class UserProfileViewset(viewsets.ModelViewSet):
     permission_classes=(permissions.UpdateOwnProfile,)
     filter_backends=(filters.SearchFilter,)
     search_fields=('name','email',)
+
+class UserLoginAPIView(ObtainAuthToken):
+    """handle user authentication tokens"""
+    renderer_classes=api_settings.DEFAULT_RENDERER_CLASSES
